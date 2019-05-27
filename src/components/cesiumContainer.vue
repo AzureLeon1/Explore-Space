f27d48fc8fe36e61670fa841ff2959a1bf55c2d5
Leon <leonwangchn@163.com>
2019-05-27 15:23:37 +0800

    地球模型加入光照

--- src/components/cesiumContainer.vue
+++ src/components/cesiumContainer.vue
@@ -1,18 +1,22 @@
 <template>
-   <div id="cesiumContainer">
-   </div>
+  <div id="cesiumContainer"></div>
 </template>
 
 <script>
-import Cesium from 'cesium/Cesium'; 
-import widgets from'cesium/Widgets/widgets.css';
+import Cesium from "cesium/Cesium";
+import widgets from "cesium/Widgets/widgets.css";
 
 export default {
-  name: 'cesiumContainer',
-  mounted(){
-  	 var viewer = new Cesium.Viewer('cesiumContainer');
+  name: "cesiumContainer",
+  mounted() {
+    var viewer = new Cesium.Viewer("cesiumContainer");
+    viewer._cesiumWidget._creditContainer.style.display = "none";
+    //  viewer._cesiumWidget._creditViewport.style.display="none";
+
+    // Enable lighting based on sun/moon positions
+    viewer.scene.globe.enableLighting = true;
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
