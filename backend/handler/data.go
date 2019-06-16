abd05d7180bea0a163c95a24dd5f5269b2db8f12
Leon <leonwangchn@163.com>
2019-06-16 12:58:18 +0800

    后端将csv转化成json

--- backend/handler/data.go
+++ backend/handler/data.go
@@ -1,9 +1,12 @@
 package handler
 
 import (
+	// "fmt"
+	// "encoding/csv"
+    // "encoding/json"
 	"os"
 	"io"
-	// "bufio"
+	"bufio"
     "net/http"
     "github.com/labstack/echo"
 
@@ -24,15 +27,38 @@ func (h* Handler) HandleData(c echo.Context) (err error) {
 	defer src.Close()
 
 	// Destination
-	dst, err := os.Create(file.Filename)
+	// dst, err := os.Create(file.Filename)
+	dst, err := os.Create("school.json")
 	if err != nil {
 		return err
 	}
 	defer dst.Close()
-	// Copy
-	if _, err = io.Copy(dst, src); err != nil {
-		return err
+
+	dst.Write([]byte("["))
+
+	// // Copy
+	// if _, err = io.Copy(dst, src); err != nil {
+	// 	return err
+	// }
+
+	br := bufio.NewReader(src)
+	bw := bufio.NewWriter(dst)
+	cnt := 0
+    for {
+		if (cnt > 0) {
+			bw.WriteString(",\n")
+		}
+		cnt++
+        a, _, c := br.ReadLine()
+        if c == io.EOF {
+            break
+        }
+		bw.WriteString(string(a))
+		bw.Flush()
 	}
+	
+	dst.Write([]byte("]"))
+
 
 	return c.String(http.StatusOK, "test")
 }
